bdcce76d78234ca22041ea4033be9f7abcc238c8
Nemoto Mailon <minatokiroisen77@gmail.com>
2025-11-25 20:08:47 +0900

    building login page layout and logic

---
+++ frontend/notes-app/src/components/Input/PasswordInput.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import { FaRegEye, FaRegEyeSlash} from 'react-icons/fa6'
+
+const PasswordInput = ({ value, placeholder, onChange }) => {
+    const [isShowPassword, setIsShowPassword] = useState(false)
+
+    const toggleShowPassword = () => {
+    setIsShowPassword(!isShowPassword)
+    }
+
+  return (
+    <div className="flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3">
+        <input
+            value={value}
+            placeholder={placeholder || "Password"}
+            onChange={onChange}
+            className="w-full text-sm bg-transparent py-3 mr-3 rounded outline-none"
+            type={isShowPassword ? "text" : "password"}
+          />
+          
+          {
+              isShowPassword ? (
+                <FaRegEye
+                size={22}
+                className="cursor-pointer text-[#2b86ff]" 
+                onClick={() => toggleShowPassword()}
+              />
+            ) : (
+              <FaRegEyeSlash
+                size={22}
+                className="cursor-pointer text-slate-400" 
+                onClick={() => toggleShowPassword()}
+              />
+            )
+          }
+    </div>
+  )
+}
+
+export default PasswordInput
--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,10 +1,43 @@
+import { Link } from 'react-router-dom'
 import Navbar from '../../components/Navbar/Navbar'
+import PasswordInput from '../../components/Input/PasswordInput'
 
 const Login = () => {
+
   return (
     <div>
       <Navbar />
-      Login
+      
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form action="" className="">
+            <h4 className="text-2xl mb-7">Login</h4>
+
+            <input
+              type="text"
+              placeholder='Email'
+              className='input-box'
+            />
+            <PasswordInput
+             
+            />
+
+            <button
+              type="submit"
+              className="btn-primary bg-[#2b86ff]"
+            >
+              Login
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              Not registered yet?{" "}
+              <Link to="/signUp" className='font-medium text-[#2b86ff] underline'>
+                Create an account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
